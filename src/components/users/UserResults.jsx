e3561ec0cfa361e0f4047445556676a12799ca41
unknown <shanelwilliams2018@gmail.com>
2023-09-01 23:04:50 -0600

    adding reducer

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,35 +1,22 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
+import { useEffect, useContext } from 'react'
 import Spinner from '../layout/Spinner'
 import UserItem from '../users/UserItem'
+import GithubContext from '../../context/github/GithubContext'
 
 function UserResults() {
-	const [users, setUsers] = useState([])
-	const [loading, setLoading] = useState(true)
+	const {users, loading, fetchUsers} = useContext(GithubContext)
 
 	useEffect(() => {
 		fetchUsers()
 	}, [])
 
-	const fetchUsers = async () => {
-		const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-			headers: {
-				Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-			},
-		})
-		const data = await response.json()
-		console.log(data)
-
-		setUsers(data)
-		setLoading(false)
-	}
-
 	if (!loading) {
 		return (
 			<div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-col-3 md:grid-col-2'>
-				{users.map((user) => (
+				{users.map((user) => 
 					<UserItem key={user.id} user={user} />
-				))}
+				)}
 			</div>
 		)
 	} else {
